1f609d2cf9dec4ddcef4fe5144b7ccb7d26f9ce3
Alexy Skoutnev <Alexyskoutnev@gmail.com>
2021-01-08 15:46:30 -0600

    Added Cost Derivative and test evaluate methods

--- Network.py
+++ Network.py
@@ -18,7 +18,6 @@ class Network(object):
         return v
     def SGN(self, training_data, epochs, mini_sample_size, eta, test_data = None):
         '''
-
         :param training_data: the training data-set
         :param epochs: Array of epochs
         :param mini_sample_size: The size of sample backpropagation
@@ -53,12 +52,57 @@ class Network(object):
         self.weights = [w - (eta/len(sample))*nw for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b - (eta / len(sample)) * nb for b, nb in zip(self.biases, nabla_b)]
 
+    def backprop(self, x, y):
+        """
+        :param x:
+        :param y:
+        :return:
+        """
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        activation = x
+        activations = [x]
+        zs = []
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = self.sigmoid(z)
+            activations.append(activation)
+        delta = self.cost_derivative(activations[-1], y) * self.sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = self.sigmoid_prime(z)
+            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
+        return (nabla_b, nabla_w)
+
+    def evaluate(self, test_data):
+        """
+        :param test_data:
+        :return:
+        """
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                        for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+    def cost_derivative(self, output_activations, y):
+        """
+        :param output_activations:
+        :param y:
+        :return:
+        """
+        return (output_activations - y)
+
     def sigmoid(self, z):
         '''
         :param z: input numpy array of size (n,1)K
         :return: output sigmoidized numpy array of size (n,1)
         '''
         return 1/(1 + np.exp(-z))
+
     def sigmoid_prime(self, z):
         """
         :param z: input numpy of size (n,1)
