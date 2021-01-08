cf93c795423bd76c80819fa48354dd063cb0eeef
Alexy Skoutnev <Alexyskoutnev@gmail.com>
2021-01-08 15:23:18 -0600

    Added Added Stochastic Gradient Descent Algorithm

--- Network.py
+++ Network.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 class Network(object):
 
     def __init__(self, sizes):
@@ -15,14 +16,55 @@ class Network(object):
         for b, w in zip(self.biases, self.weights):
             v = self.sigmoid(np.dot(w, v) + b)
         return v
-    def SGN(self):
-        pass
+    def SGN(self, training_data, epochs, mini_sample_size, eta, test_data = None):
+        '''
+
+        :param training_data: the training data-set
+        :param epochs: Array of epochs
+        :param mini_sample_size: The size of sample backpropagation
+        :param eta: learning rate
+        :param test_data: the test data-set
+        :return: finds exterma of cost function
+        '''
+        if test_data:
+            n_test = len(test_data)
+        n_train = len(training_data)
+        for i in range(epochs):
+            random.shuffle(training_data)
+            mini_samples = [training_data[j:j+mini_sample_size] for j in range(0, n_test, mini_sample_size)]
+            for sample in mini_samples:
+                self.update_mini_sample(sample, eta)
+            if test_data:
+                print("Epoch {0}: {1} / {2}".format(i, self.evualate(test_data)/ n_test))
+            else:
+                print("Epoch {0} complete".format(i))
+    def update_mini_sample(self, sample, eta):
+        """
+        :param sample: sample from mini sample
+        :param eta: learning rate
+        :return: updates biases and weights of each node
+        """
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in sample:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w - (eta/len(sample))*nw for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b - (eta / len(sample)) * nb for b, nb in zip(self.biases, nabla_b)]
+
     def sigmoid(self, z):
         '''
-        :param z: input numpy array of size (n,1)
+        :param z: input numpy array of size (n,1)K
         :return: output sigmoidized numpy array of size (n,1)
         '''
         return 1/(1 + np.exp(-z))
+    def sigmoid_prime(self, z):
+        """
+        :param z: input numpy of size (n,1)
+        :return: output derivative of sigmoidized numpy array of size (n,1)
+        """
+        return self.sigmoid(z)*(1 - self.sigmoid(z))
 
 network = Network([2,3,2])
 # print(network.biases)
