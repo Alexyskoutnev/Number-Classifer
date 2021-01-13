002dabeb0e39d7fdecd2e0c15c6e6013e6716e9f
Alexy Skoutnev <Alexyskoutnev@gmail.com>
2021-01-13 12:49:14 -0600

    Now the algorithm appoximates the numbers correctly, only thing the neural network works kinda slow

--- Read_Digit.py
+++ Read_Digit.py
@@ -3,22 +3,22 @@ import cv2
 import numpy as np
 import time
 from scipy import ndimage
+import math
 
 def read(Network):
-    array_digits_data = []
     camera = cv2.VideoCapture('http://10.0.0.198:8080/video')
     Lower_Black = np.array([0, 0, 0])
     Upper_Black = np.array([100, 100, 100])
     center = None
     while True:
-        # img = cv2.imread('Multiple_Numbers.jpg',1)
         (_, img) = camera.read()
+        #img = cv2.imread('Sample_Number_3.jpg', 1)
         #img = cv2.flip(img, 1)
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        kernel = np.ones((3, 3), np.uint8)
+        kernel = np.ones((5, 5), np.uint8)
         _, thresh = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
         thresh = cv2.erode(thresh, kernel, iterations=1)
-        thresh = cv2.dilate(thresh, kernel, iterations=1)
+        thresh = cv2.dilate(thresh, kernel, iterations=2)
         thresh = cv2.erode(thresh, kernel, iterations=1)
         # mask_inrange = cv2.inRange(hsv, Lower_Black, Upper_Black)
         # #mask_morph_open = cv2.morphologyEx(mask_inrange, cv2.MORPH_OPEN, kernel)
@@ -42,23 +42,25 @@ def read(Network):
                 filename = 'EXAMPLE_OUTPUT.jpg'
                 x, y, h, w = Rectangle
                 digit_image = img[y:y + w, x:x + h]
-                gray_digit_image = cv2.cvtColor(digit_image, cv2.COLOR_BGR2GRAY)
-                gray_digit_image = cv2.resize(gray_digit_image, (28, 28))
-                digit_data = np.array(gray_digit_image)
+                img_processed = process(digit_image)
+                # gray_digit_image = cv2.cvtColor(digit_image, cv2.COLOR_BGR2GRAY)
+                # gray_digit_image = cv2.resize(gray_digit_image, (28, 28))
+                #digit_data = np.array(img_processed)
                 # digit_data = process(digit_image)
                 # cv2.imshow("porcess", digit_data)
-                digit_data = digit_data.astype('float32').flatten() / 255.0
+                digit_data = (img_processed.flatten())/255.0
                 digit_data_new = digit_data.reshape(784, 1)
                 #cv2.imwrite(filename, gray_digit_image)
-                array_digits_data.append(digit_data_new)
                 #cv2.imshow("Final Image111", gray_digit_image)
                 output_digit = np.argmax(Network.feedforward(digit_data_new))
+
                 cv2.rectangle(img, (x, y), (x + h, y + w), (0, 255, 0), 2)
                 cv2.putText(img, str(output_digit), (x + h, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
         except:
+            print("broke")
             pass
         #Show Image
-        resized_image = cv2.resize(img, (800, 800))
+        resized_image = cv2.resize(img, (1000, 1000))
         cv2.imshow("Digits Recognition Real Time", resized_image)
 
         #Break Image
@@ -67,6 +69,7 @@ def read(Network):
 
     #Turn off Camera
     camera.release()
+    #cv2.waitKey()
     cv2.destroyWindow()
 
 def get_rectangles(cont, hier):
@@ -80,86 +83,52 @@ def get_rectangles(cont, hier):
     common_heirarchy = u[np.argmax(np.bincount(indices))]
     for rec, hr in zip(rectangles, hier):
         x,y,w,h = rec
-        if ((w*h) > 300) and (20 <= w <= 200) and (20 <= h <= 200) and (hr[3] == common_heirarchy):
+        if ((w*h) > 500) and (30 <= w <= 200) and (30 <= h <= 200) and (hr[3] == common_heirarchy):
             digit_rectangles.append(rec)
     return digit_rectangles
 
-# def process(img):
-#     digit_image = -np.ones(img.shape)
-#
-#     height, width = img.shape
-#
-#     predSet_ret = []
-#
-#     """
-#     crop into several images
-#     """
-#     for cropped_width in range(100, 300, 20):
-#         for cropped_height in range(100, 300, 20):
-#             for shift_x in range(0, width - cropped_width, int(cropped_width / 4)):
-#                 for shift_y in range(0, height - cropped_height, int(cropped_height / 4)):
-#                     gray = img[shift_y:shift_y + cropped_height, shift_x:shift_x + cropped_width]
-#                     if np.count_nonzero(gray) <= 20:
-#                         continue
-#
-#                     if (np.sum(gray[0]) != 0) or (np.sum(gray[:, 0]) != 0) or (np.sum(gray[-1]) != 0) or (np.sum(gray[:,
-#                                                                                                                  -1]) != 0):
-#                         continue
-#
-#                     top_left = np.array([shift_y, shift_x])
-#                     bottom_right = np.array([shift_y + cropped_height, shift_x + cropped_width])
-#
-#                     while np.sum(gray[0]) == 0:
-#                         top_left[0] += 1
-#                         gray = gray[1:]
-#
-#                     while np.sum(gray[:, 0]) == 0:
-#                         top_left[1] += 1
-#                         gray = np.delete(gray, 0, 1)
-#
-#                     while np.sum(gray[-1]) == 0:
-#                         bottom_right[0] -= 1
-#                         gray = gray[:-1]
-#
-#                     while np.sum(gray[:, -1]) == 0:
-#                         bottom_right[1] -= 1
-#                         gray = np.delete(gray, -1, 1)
-#
-#                     actual_w_h = bottom_right - top_left
-#                     if (np.count_nonzero(digit_image[top_left[0]:bottom_right[0], top_left[1]:bottom_right[1]] + 1) >
-#                             0.2 * actual_w_h[0] * actual_w_h[1]):
-#                         continue
-#
-#                     rows, cols = gray.shape
-#                     compl_dif = abs(rows - cols)
-#                     half_Sm = int(compl_dif / 2)
-#                     half_Big = half_Sm if half_Sm * 2 == compl_dif else half_Sm + 1
-#                     if rows > cols:
-#                         gray = np.lib.pad(gray, ((0, 0), (half_Sm, half_Big)), 'constant')
-#                     else:
-#                         gray = np.lib.pad(gray, ((half_Sm, half_Big), (0, 0)), 'constant')
-#
-#                     gray = cv2.resize(gray, (20, 20))
-#                     gray = np.lib.pad(gray, ((4, 4), (4, 4)), 'constant')
-#
-#                     shiftx, shifty = getBestShift(gray)
-#                     shifted = shift(gray, shiftx, shifty)
-#                     gray = shifted
-#     return gray
-#
-# def shift(img, sx, sy):
-#     rows, cols = img.shape
-#     M = np.float32([[1, 0, sx], [0, 1, sy]])
-#     shifted = cv2.warpAffine(img, M, (cols, rows))
-#     return shifted
-#
-# def getBestShift(img):
-#     cy,cx = ndimage.measurements.center_of_mass(img)
-#     print(cy,cx)
-#
-#     rows,cols = img.shape
-#     shiftx = np.round(cols/2.0-cx).astype(int)
-#     shifty = np.round(rows/2.0-cy).astype(int)
-#     return shiftx, shifty
+
+def process(img):
+    gray_digit_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thresh, gray_digit_image = cv2.threshold(gray_digit_image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    gray_digit_image = cv2.resize(255 - gray_digit_image, (28, 28))
+    rows, cols = gray_digit_image.shape
+
+    if rows > cols:
+        factor = 20.0 / rows
+        rows = 20
+        cols = int(round(cols * factor))
+        gray_digit_image = cv2.resize(gray_digit_image, (cols, rows))
+    else:
+        factor = 20.0 / cols
+        cols = 20
+        rows = int(round(rows * factor))
+        gray_digit_image = cv2.resize(gray_digit_image, (cols, rows))
+
+    #Add the missing corner pixels
+    colsPadding = (int(math.ceil((28 - cols) / 2.0)), int(math.floor((28 - cols) / 2.0)))
+    rowsPadding = (int(math.ceil((28 - rows) / 2.0)), int(math.floor((28 - rows) / 2.0)))
+    gray_digit_image = np.lib.pad(gray_digit_image, (rowsPadding, colsPadding), 'constant')
+
+    shiftx, shifty = getBestShift(gray_digit_image)
+    shifted = shift(gray_digit_image, shiftx, shifty)
+    gray_digit_image = shifted
+
+    return gray_digit_image
+
+def shift(img, sx, sy):
+    rows, cols = img.shape
+    M = np.float32([[1, 0, sx], [0, 1, sy]])
+    shifted = cv2.warpAffine(img, M, (cols, rows))
+    return shifted
+
+def getBestShift(img):
+    M = cv2.moments(img)
+    cx = int(M["m10"] / M["m00"])
+    cy = int(M["m01"] / M["m00"])
+    rows, cols = img.shape
+    shiftx = np.round(cols / 2.0 - cx).astype(int)
+    shifty = np.round(rows/2.0-cy).astype(int)
+    return shiftx, shifty
 
 
